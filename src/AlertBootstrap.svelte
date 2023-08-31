e86260a74613cf1bd856e4061a1450ac67a30aba
Pablo Carli <pablodcarli@gmail.com>
2023-08-31 14:18:22 -0300

    se suma alertbootstrap y cambios en main

---
+++ src/AlertBootstrap.svelte
@@ -0,0 +1,17 @@
+<script>
+	
+	export let alertas = [];
+	
+</script>
+
+
+		
+<ul>
+	{#each alertas as { contenido, estilo }, i}
+		<div class="alert alert-primary" role="alert">
+			A simple primary alert—check it out!
+		</div>
+	{/each} 
+</ul>
+		
+		
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import App from './App.svelte';
+import AlertBootstrap from './AlertBootstrap.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+new AlertBootstrap({target: document.body, props:{
+	alertas :[
+		{ estilo: 'primary', contenido: 'mensaje ' },
+		{ estilo: 'danger', contenido: 'mensaje ' },
+		{ estilo: 'danger', contenido: 'mensaje ' },
+		{ estilo: 'warning', contenido: 'mensaje ' }
+	]
+}});
+
+
 
 export default app;
+export default AlertBootstrap;
